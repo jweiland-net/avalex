0024b9ee7ea4cdebc4ce2fad8253a32346790b00
Pascal Rinker <info@crynton.com>
2018-05-15 16:08:21 +0200

    [TASK] Remove concat from ext_emconf for TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'avalex',
-    'description' =>
-        'The Avalex extension allows to display an automatically generated and '
-        . 'updated „Data Privacy Statement” within a TYPO3 web site.',
+    'description' => 'The Avalex extension allows to display an automatically generated and updated „Data Privacy Statement” within a TYPO3 web site.',
     'category' => 'plugin',
     'author' => '',
     'author_email' => '',
