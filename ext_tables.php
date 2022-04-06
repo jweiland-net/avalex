c5c42afdfcae15cfae6ee02d17028ccc2d4866f2
Pascal Rinker <pascal20997@users.noreply.github.com>
2022-04-06 12:25:38 +0200

    [TASK] Remove unused table from allowTableOnStandardPages
    Thank you @froemken 😉

--- ext_tables.php
+++ ext_tables.php
@@ -2,4 +2,3 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_avalex_configuration');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_avalex_legaltext');
