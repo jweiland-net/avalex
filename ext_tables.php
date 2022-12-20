c5892434f0b4d1955186e838a2a7fbe7cc9e9518
Stefan Froemken <froemken@gmail.com>
2022-12-20 20:33:00 +0100

    Add check for TYPO3 constant

--- ext_tables.php
+++ ext_tables.php
@@ -1,4 +1,6 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+if (!defined('TYPO3_MODE') && !defined('TYPO3')) {
+    die('Access denied.');
+}
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_avalex_configuration');
