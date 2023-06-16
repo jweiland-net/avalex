8c0738f5fb7ec39f0c68ffb0892314494a982a4b
Stefan Froemken <sfroemken@jweiland.net>
2023-06-16 09:07:34 +0200

    Update lines mentioned by php-cs-fixer

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -7,18 +7,18 @@ if (!defined('TYPO3_MODE') && !defined('TYPO3')) {
 return [
     'avalex_avalex' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:avalex/Resources/Public/Icons/avalex_avalex.svg'
+        'source' => 'EXT:avalex/Resources/Public/Icons/avalex_avalex.svg',
     ],
     'avalex_imprint' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:avalex/Resources/Public/Icons/avalex_imprint.svg'
+        'source' => 'EXT:avalex/Resources/Public/Icons/avalex_imprint.svg',
     ],
     'avalex_bedingungen' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:avalex/Resources/Public/Icons/avalex_bedingungen.svg'
+        'source' => 'EXT:avalex/Resources/Public/Icons/avalex_bedingungen.svg',
     ],
     'avalex_widerruf' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:avalex/Resources/Public/Icons/avalex_widerruf.svg'
+        'source' => 'EXT:avalex/Resources/Public/Icons/avalex_widerruf.svg',
     ],
 ];
