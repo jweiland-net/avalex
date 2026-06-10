84b17aa96339434b062f73bd79241434912325b3
Stefan Froemken <sfroemken@jweiland.net>
2026-06-10 16:26:40 +0200

    [TASK] Adjust Rector configuration for TYPO3 13.4 compatibility
    Lower TYPO3 version constraint and Rector level set to support TYPO3 13.4 and later. This ensures broader compatibility and aligns with updated requirements.

--- Build/rector/rector.php
+++ Build/rector/rector.php
@@ -39,11 +39,11 @@ return RectorConfig::configure()
     ->withSets([
         Typo3SetList::CODE_QUALITY,
         Typo3SetList::GENERAL,
-        Typo3LevelSetList::UP_TO_TYPO3_14,
+        Typo3LevelSetList::UP_TO_TYPO3_13,
     ])
     ->withPHPStanConfigs([Typo3Option::PHPSTAN_FOR_RECTOR_PATH])
     ->withConfiguredRule(ExtEmConfRector::class, [
-        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '14.3.0-14.3.99',
+        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '13.4.0-14.3.99',
         ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
     ])
     ->withConfiguredRule(EncapsedStringsToSprintfRector::class, [
