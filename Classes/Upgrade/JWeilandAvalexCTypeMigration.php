e0f30a31e435ce9a5303e70b13e0e292bc09ebf3
Stefan Froemken <sfroemken@jweiland.net>
2026-06-12 10:32:53 +0200

    [TASK] Add strict types, update XLIFF versions, and adjust UpgradeWizard annotations
    Introduced `declare(strict_types=1)` for improved type safety across files. Upgraded XLIFF files from version 1.0 to 1.2 for better compliance. Enhanced UpgradeWizard attributes with a clearer annotation style for improved readability.

--- Classes/Upgrade/JWeilandAvalexCTypeMigration.php
+++ Classes/Upgrade/JWeilandAvalexCTypeMigration.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the package jweiland/avalex.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace JWeiland\Avalex\Upgrade;
 
 use TYPO3\CMS\Core\Attribute\UpgradeWizard;
 use TYPO3\CMS\Core\Upgrades\AbstractListTypeToCTypeUpdate;
 
-#[UpgradeWizard('jweilandAvalexCTypeMigration')]
+#[UpgradeWizard(
+    identifier: 'jweilandAvalexCTypeMigration',
+)]
 final class JWeilandAvalexCTypeMigration extends AbstractListTypeToCTypeUpdate
 {
     protected function getListTypeToCTypeMapping(): array
